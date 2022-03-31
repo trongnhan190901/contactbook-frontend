1394d2f4cf164ff5bbade9f0e869e7ddb6213552
Trọng Nhân <58482915+trongnhan190901@users.noreply.github.com>
2022-03-31 19:06:51 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    port: 8081,
+    proxy: {
+     
+        "/api": {
+          target: "http://localhost:8080/",
+          changeOrigin: true,
+        },
+      
+    }
+},
 })
